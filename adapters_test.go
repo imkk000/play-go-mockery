c4ec3b69aaae6747b39724dd650a699893b29e87
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2023-11-16 12:17:48 +0700

    Add custom clean up function

--- adapters_test.go
+++ adapters_test.go
@@ -3,16 +3,16 @@ package mocks_test
 import (
 	"mocks/mocks"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAdapters(t *testing.T) {
+	t.Cleanup(func() {
+		t.Error("clean up stuff")
+	})
+
 	mockExt := mocks.NewExternalAdapter(t)
 	mockInt := mocks.NewInternalAdapter(t)
 
 	mockExt.On("Do", "ping").Return("pong", nil)
 	mockInt.On("Do").Return(true, nil)
-
-	assert.True(t, mockExt.AssertNotCalled(t, "Do", "ping"))
 }
